12e85c08c0c12fc9b0fe86ef2ee6541227972eed
Alexandre Frade <kernel@xanmod.org>
2023-05-19 16:49:47 +0800

    kernel/sched/fair: Optimize fair
    * Set scheduler tunable latencies to unscaled
    * Increase wake-up granularity time to 3ms

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -111,7 +111,7 @@ unsigned int sysctl_sched_cstate_aware = 1;
 #ifdef CONFIG_SCHED_BORE
 enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 #else // CONFIG_SCHED_BORE
-enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_LOG;
+enum sched_tunable_scaling sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_NONE;
 #endif // CONFIG_SCHED_BORE
 
 /*
@@ -153,8 +153,8 @@ unsigned int sysctl_sched_child_runs_first __read_mostly = 1;
 unsigned int sysctl_sched_wakeup_granularity		= 4000000UL;
 unsigned int normalized_sysctl_sched_wakeup_granularity	= 4000000UL;
 #else // CONFIG_SCHED_BORE
-unsigned int sysctl_sched_wakeup_granularity		= 1000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity	= 1000000UL;
+unsigned int sysctl_sched_wakeup_granularity		= 3000000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity	= 3000000UL;
 #endif // CONFIG_SCHED_BORE
 
 const_debug unsigned int sysctl_sched_migration_cost	= 1000000UL;
